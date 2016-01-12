e14f25a74a1934dca287bbdc20146669fec1e87b
songchen.sxc <songchen.sxc@taobao.com>
2016-01-12 10:30:20 +0800

    调整模块结构

--- mods/blur/index.js
+++ mods/blur/index.js
@@ -6,7 +6,7 @@
 var Promise = require('bluebird');
 var Canvas = require('canvas');
 var Image = Canvas.Image;
-var ModUtils = require('../common/utils');
+var MyUtils = require('../../common/utils');
 
 var am = {
   mul_table: [
@@ -18,7 +18,6 @@ var am = {
   /**
    * 处理图片
    * @param imgData
-   * @param imgSrc
    * @param size
    * @param radius
    */
@@ -32,7 +31,7 @@ var am = {
       originImg.onload = function() {
 
         // 原图尺寸
-        var compressedSizeObj = ModUtils.genCompressedImgSize(originImg.width, originImg.height, size, size);
+        var compressedSizeObj = MyUtils.genCompressedImgSize(originImg.width, originImg.height, size, size);
 
         // 压缩后图片宽高
         var w = compressedSizeObj.width;
