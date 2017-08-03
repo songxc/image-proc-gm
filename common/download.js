60aa12d7ae4a2bb80e0f4bbebcddff689428ddc2
songchen.sxc <songchen.sxc@taobao.com>
2017-08-03 22:34:36 +0800

    update module download

--- common/download.js
+++ common/download.js
@@ -3,13 +3,13 @@
  * 下载文件
  */
 var Path = require('path');
-var Download = require('download');
+var download = require('download');
 var Promise = require('bluebird');
 
 module.exports = {
   /**
    * 下载文件
-   * @param srcUrl 源地址（支持数组）
+   * @param srcUrl 源地址
    * @param destDir 目标目录
    * @returns {bluebird|exports|module.exports}
    */
@@ -26,14 +26,12 @@ module.exports = {
       var filePath = Path.join(destDir, fileName);
 
       // 下载图片
-      new Download({mode: '755'})
-        .get(srcUrl, destDir)
-        .run(function(err, files) { // files 是个数组, 内容结构参考: http://stackoverflow.com/questions/22286900/how-do-i-interact-with-this-file-object-in-a-node-js-stream
-          if (err) {
-            reject(err);
-            return;
-          }
+      download(srcUrl, destDir)
+        .then(function() {
           resolve(filePath);
+        })
+        .catch(function(err) {
+          reject(err);
         });
     });
   }
